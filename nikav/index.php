a47947dbe43b9e5c4e12ff75fa4dcab1ab9d9e5c
Nika Volk <76059172+nikavolk@users.noreply.github.com>
2025-03-28 10:50:09 +0100

    Attempt to deploy using Github acctions

---
+++ nikav/index.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sample_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+$users = [];
+while ($row = $result->fetch_assoc()) {
+    $users[] = $row;
+}
+
+header('Content-Type: application/json');
+echo json_encode($users);
+
+$conn->close();
+?>
