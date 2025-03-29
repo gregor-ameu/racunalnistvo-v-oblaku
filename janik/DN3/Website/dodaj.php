744c347729a10f7151ff41a1e8d07192f01b08d4
ALMAkojc525 <jani.kojc1@almamater.si>
2025-03-29 18:10:46 +0100

    Domača naloga 3
    Oddaja domače naloge 3: spletna stran "O meni" na apache2 strežniku

---
+++ janik/DN3/Website/dodaj.php
@@ -0,0 +1,16 @@
+<?php
+$data = json_decode(file_get_contents("php://input"), true);
+$aktivnost = $data["aktivnost"];
+$znanje = $data["znanje"];
+$cas = $data["cas"];
+
+$conn = new mysqli("10.0.0.24", "kojc", "Maco5255252121", "moja_baza");
+if ($conn->connect_error) die("Napaka pri povezavi: " . $conn->connect_error);
+
+$stmt = $conn->prepare("INSERT INTO aktivnosti (aktivnost, znanje, cas) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $aktivnost, $znanje, $cas);
+$stmt->execute();
+
+echo "Dodano.";
+$conn->close();
+?>
